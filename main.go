1dc067702445a8352fea9c4183cdc43d57ecc36b
pmt <patriciatorvalds@gmail.com>
2018-07-18 11:56:22 -0400

    Moved file structure, now they all talk

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/windmilleng/PETS/cmd/pets"
 )
 
 func main() {
-	if err := RootCmd.Execute(); err != nil {
+	if err := pets.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
