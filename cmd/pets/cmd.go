320bc624713678426b038f19c7cc9d68376c22a1
pmt <patriciatorvalds@gmail.com>
2018-07-19 12:14:27 -0400

    Renamed successfully

--- cmd/pets/cmd.go
+++ cmd/pets/cmd.go
@@ -12,17 +12,16 @@ import (
 
 func init() {
 	RootCmd.AddCommand(DryRunCmd)
-	// 	 rootCmd.PersistentFlags().String("dry-run")
 }
 
 var RootCmd = &cobra.Command{
 	Use:   "pets [arguments]",
 	Short: "PETS makes it easy to manage lots of servers running on your machine that you want to keep a close eye on for local development.",
 	Long: `A PETS file is like a Makefile for running servers and connecting them 
-			to other servers. With PETS, we can switch back and forth quickly 
-			between servers running locally and servers running in the cloud.`,
+	to other servers. With PETS, we can switch back and forth quickly
+	between servers running locally and servers running in the cloud.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello :)")
+		fmt.Println("You run pets!")
 	},
 }
 
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/windmilleng/PETS/cmd/pets"
+	"github.com/windmilleng/pets/cmd/pets"
 )
 
 func main() {
