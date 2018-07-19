62aac1a3a3a40f9f3eb9f8d2a44d1c8145bfb5c0
Han Yu <han@sent.com>
2018-07-19 16:03:26 -0400

    Parse Petsfile to Print (#8)
    * mill: skylark interpreter parses Petsfile for print func
    
    * mill: add skylark vendor dep

---
+++ internal/mill/exec.go
@@ -0,0 +1,26 @@
+package mill
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/google/skylark"
+)
+
+// ExecFile takes a Petsfile and parses it using the Skylark interpreter
+func ExecFile(file string, stdout io.Writer) error {
+	thread := &skylark.Thread{
+		Print: func(_ *skylark.Thread, msg string) { fmt.Fprintln(stdout, msg) },
+	}
+	_, err := skylark.ExecFile(thread, file, nil, nil)
+	return err
+}
+
+func getFilePath() string {
+	const Petsfile = "Petsfile"
+	cwd, _ := os.Getwd()
+
+	return filepath.Join(cwd, Petsfile)
+}
