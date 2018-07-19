b6543cfad39c5b82677a8939f7e4a9220a303324
Han Yu <han@windmill.engineering>
2018-07-19 15:21:15 -0400

    mill: skylark interpreter parses Petsfile for print func

---
+++ internal/mill/exec_test.go
@@ -0,0 +1,38 @@
+package mill
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestExecFile(t *testing.T) {
+	stdout := &bytes.Buffer{}
+	dir, _ := ioutil.TempDir("", t.Name())
+	file := filepath.Join(dir, "Millfile")
+	ioutil.WriteFile(file, []byte(`print("hello")`), os.FileMode(0777))
+	ExecFile(file, stdout)
+	defer os.RemoveAll(dir)
+
+	out := stdout.String()
+	if out != "hello\n" {
+		t.Errorf("Expected 'hello'. Actual: %s", out)
+	}
+}
+
+func TestExecFileFail(t *testing.T) {
+	stdout := &bytes.Buffer{}
+	dir, _ := ioutil.TempDir("", t.Name())
+	file := filepath.Join(dir, "Millfile")
+	ioutil.WriteFile(file, []byte(`print(hello)`), os.FileMode(0777))
+	defer os.RemoveAll(dir)
+
+	err := ExecFile(file, stdout)
+	out := stdout.String()
+	if !(out == "" && strings.Contains(err.Error(), "undefined: hello")) {
+		t.Errorf("Expected 'hello'. Actual: %s. Err: %s", out, err)
+	}
+}
