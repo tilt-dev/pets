3fa8ec925d38e8410db0e7ef951d0aa2b7552d58
Nick Santos <nicholas.j.santos@gmail.com>
2018-07-27 18:46:51 -0400

    proc: add service keys to procfs

--- internal/proc/proc.go
+++ internal/proc/proc.go
@@ -5,6 +5,8 @@ import (
 	"os/exec"
 	"syscall"
 	"time"
+
+	"github.com/windmilleng/pets/internal/service"
 )
 
 // Process state that we expect to be written to disk and queried.
@@ -23,6 +25,10 @@ type PetsProc struct {
 
 	// The port that the process is listening on (e.g., '8080')
 	Port int `json:",omitempty"`
+
+	// The name+tier of the service that this process exposes
+	ServiceName service.Name `json:",omitempty"`
+	ServiceTier service.Tier `json:",omitempty"`
 }
 
 // Creates a new PetsProc that we know is listening on the given host and port.
@@ -32,6 +38,16 @@ type PetsProc struct {
 func (p PetsProc) WithExposedHost(hostname string, port int) PetsProc {
 	p.Hostname = hostname
 	p.Port = port
+	return p
+}
+
+// Creates a new PetsProc that matches a service key.
+//
+// Calling this method automatically creates a copy because it's a struct method
+// rather than a pointer method.
+func (p PetsProc) WithServiceKey(key service.Key) PetsProc {
+	p.ServiceName = key.Name
+	p.ServiceTier = key.Tier
 	return p
 }
 
